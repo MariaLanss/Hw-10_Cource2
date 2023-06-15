1b9fadeb09b125d8b945f777993482be7f572067
MariaLanss <maruska0990@gmail.com>
2023-06-15 20:30:05 +0300

    .

---
+++ src/main/java/pro/sky/hw10_cource2/Hw10Cource2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.hw10_cource2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw10Cource2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw10Cource2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/hw10_cource2/Hw10Cource2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hw10_cource2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw10Cource2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
